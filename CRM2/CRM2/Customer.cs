4efd228e27e9fa61b445e35959cd4d81ee3d1892
Neohack00 <neo@bylock.se>
2019-09-23 18:03:52 +0200

    Added Class: Customer

---
+++ CRM2/CRM2/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM2
+{
+    class Customer
+    {
+        public string Fullname { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+    }
+}
